bebfb73ceb5a31da73ebfcd0de2a116081c736ab
mdyeasinhn <mdyeasinhn@gmail.com>
2025-02-16 16:05:34 +0600

    Fixing the layout and setting up basic sidebar options

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,50 +1,75 @@
-import { Layout, Menu } from "antd";
+import { Layout, Menu, MenuProps } from "antd";
 const { Header, Content, Footer, Sider } = Layout;
-import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
-import { createElement } from "react";
 
 
-const items = [UserOutlined, VideoCameraOutlined, UploadOutlined, UserOutlined].map(
-    (icon, index) => ({
-      key: String(index + 1),
-      icon: createElement(icon),
-      label: `nav ${index + 1}`,
-    }),
-  );
+
+const items: MenuProps['items'] = [
+    {
+        key: '1',
+        label: "Dashboard",
+    },
+    {
+        key: '2',
+        label: "Profile",
+    },
+    {
+        key: '3',
+        label: "User Management",
+        children: [
+            {
+                key: '11',
+                label: "Create Admin",
+            },
+            {
+                key: '12',
+                label: "Create Student",
+            },
+        ]
+    },
+]
 const MainLayout = () => {
     return (
-        <Layout>
-        <Sider
-          breakpoint="lg"
-          collapsedWidth="0"
-          onBreakpoint={(broken) => {
-            console.log(broken);
-          }}
-          onCollapse={(collapsed, type) => {
-            console.log(collapsed, type);
-          }}
-        >
-          <div className="demo-logo-vertical" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
-        </Sider>
-        <Layout>
-          <Header style={{ padding: 0,}} />
-          <Content style={{ margin: '24px 16px 0' }}>
-            <div
-              style={{
-                padding: 24,
-                minHeight: 360,
-               
-              }}
+        <Layout style={{ height: "100vh" }}>
+            <Sider
+                breakpoint="lg"
+                collapsedWidth="0"
+                onBreakpoint={(broken) => {
+                    console.log(broken);
+                }}
+                onCollapse={(collapsed, type) => {
+                    console.log(collapsed, type);
+                }}
             >
-              Tha main content should go here
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>
-            Ant Design ©{new Date().getFullYear()} Created by Ant UED
-          </Footer>
+                <div style={
+                    {
+                        color: "white",
+                        height: "4rem",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}>
+                    <h1 >PH Uni</h1>
+                </div>
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']} items={items} />
+            </Sider>
+            <Layout>
+                <Header style={{ padding: 0, }} />
+                <Content style={{ margin: '24px 16px 0' }}>
+                    <div
+                        style={{
+                            padding: 24,
+                            minHeight: 360,
+
+                        }}
+                    >
+                        Tha main content should go here
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
+                </Footer>
+            </Layout>
         </Layout>
-      </Layout>
     );
 };
 
